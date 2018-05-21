c99e7bd593579feaa272024290fa42ae640800a6
Alexandre Afonso <afonsoaaf@gmail.com>
2018-05-21 20:02:38 -0300

    Aula gravada.

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { InputTextModule } from 'primeng/inputtext';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    InputTextModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
